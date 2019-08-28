a81949c3398cccf217b10d4ab7d84bfa30ed4cb3
ivanrazo <54483334+ivanrazo@users.noreply.github.com>
2019-08-27 20:10:35 -0500

    Add files via upload

---
+++ operadores.py
@@ -0,0 +1,15 @@
+a= int(input('primer valor: '))
+b= int(input('segundo valor: '))
+print('El resultado es: '+ str(a+b))
+
+a= int(input('primer valor: '))
+b= int(input('segundo valor: '))
+print('El resultado es: '+ str(a-b))
+
+a= int(input('primer valor: '))
+b= int(input('segundo valor: '))
+print('El resultado es: '+ str(a/b))
+
+a= int(input('primer valor: '))
+b= int(input('segundo valor: '))
+print('El resultado es: '+ str(a*b))
